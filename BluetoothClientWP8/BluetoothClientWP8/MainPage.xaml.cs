9ad589c97bef398149f1cc8ee801f0e4bb2e0071
Anton Lenev <m0rg0t.Anton@gmail.com>
2014-10-18 02:37:43 +0400

    Connecting to arduino

---
+++ BluetoothClientWP8/BluetoothClientWP8/MainPage.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using BluetoothClientWP8.Resources;
+using Windows.Networking.Sockets;
+using Windows.Networking.Proximity;
+using System.Diagnostics;
+using Windows.Storage.Streams;
+using System.Threading.Tasks;
+using BluetoothConnectionManager;
+using System.Windows.Media;
+
+namespace BluetoothClientWP8
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private ConnectionManager connectionManager;
+
+        private StateManager stateManager;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            connectionManager = new ConnectionManager();
+            connectionManager.MessageReceived += connectionManager_MessageReceived;
+            stateManager = new StateManager();
+
+        }
+
+        async void connectionManager_MessageReceived(string message)
+        {
+            Debug.WriteLine("Message received:" + message);
+            string[] messageArray = message.Split(':');
+            Dispatcher.BeginInvoke(delegate()
+            {
+                BodyDetectionStatus.Text = message;
+                BodyDetectionStatus.Foreground = new SolidColorBrush(Colors.White);
+            });
+            /*switch (messageArray[0])
+            {
+                case "LED_RED":
+                    stateManager.RedLightOn = messageArray[1] == "ON" ? true : false;
+                    Dispatcher.BeginInvoke(delegate()
+                    {
+                        RedButton.Background = stateManager.RedLightOn ? new SolidColorBrush(Colors.Red) : new SolidColorBrush(Colors.Black);
+                    });
+                break;
+                case "LED_GREEN":
+                    stateManager.GreenLightOn = messageArray[1] == "ON" ? true : false;
+                    Dispatcher.BeginInvoke(delegate()
+                    {
+                        GreenButton.Background = stateManager.GreenLightOn ? new SolidColorBrush(Colors.Green) : new SolidColorBrush(Colors.Black);
+                    });
+                break;
+                case "LED_YELLOW":
+                    stateManager.YellowLightOn = messageArray[1] == "ON" ? true : false;
+                    Dispatcher.BeginInvoke(delegate()
+                    {
+                        YellowButton.Background = stateManager.YellowLightOn ? new SolidColorBrush(Colors.Yellow) : new SolidColorBrush(Colors.Black);
+                    });
+                break;
+                case "PROXIMITY":
+                    stateManager.BodyDetected = messageArray[1] == "DETECTED" ? true : false;
+                    if (stateManager.BodyDetected)
+                    {
+                        Dispatcher.BeginInvoke(delegate()
+                        {
+                            BodyDetectionStatus.Text = "Intruder detected!!!";
+                            BodyDetectionStatus.Foreground = new SolidColorBrush(Colors.Red);
+                        });
+                        await connectionManager.SendCommand("TURN_ON_RED");
+                    } else {
+                        Dispatcher.BeginInvoke(delegate()
+                        {
+                            BodyDetectionStatus.Text = "No body detected";
+                            BodyDetectionStatus.Foreground = new SolidColorBrush(Colors.White);
+                        });
+                    }
+                    break;
+            }*/
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            connectionManager.Initialize();
+            stateManager.Initialize();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            connectionManager.Terminate();
+        }
+
+        private void ConnectAppToDeviceButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            AppToDevice();
+        }
+
+        private async void AppToDevice()
+        {
+            ConnectAppToDeviceButton.Content = "Connecting...";
+            PeerFinder.AlternateIdentities["Bluetooth:Paired"] = "";
+            var pairedDevices = await PeerFinder.FindAllPeersAsync();
+
+            if (pairedDevices.Count == 0)
+            {
+                Debug.WriteLine("No paired devices were found.");
+            }
+            else
+            { 
+                foreach (var pairedDevice in pairedDevices)
+                {
+                    if (pairedDevice.DisplayName == DeviceName.Text)
+                    {
+                        connectionManager.Connect(pairedDevice.HostName);
+                        ConnectAppToDeviceButton.Content = "Connected";
+                        DeviceName.IsReadOnly = true;
+                        ConnectAppToDeviceButton.IsEnabled = false;
+                        continue;
+                    }
+                }
+            }
+        }
+
+        private async void RedButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            string command = stateManager.RedLightOn ? "TURN_OFF_RED" : "TURN_ON_RED";
+            await connectionManager.SendCommand(command);
+        }
+
+        private async void GreenButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            string command = stateManager.GreenLightOn ? "TURN_OFF_GREEN" : "TURN_ON_GREEN";
+            await connectionManager.SendCommand(command);
+        }
+
+        private async void YellowButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            string command = stateManager.YellowLightOn ? "TURN_OFF_YELLOW" : "TURN_ON_YELLOW";
+            await connectionManager.SendCommand(command);
+        }
+    }
+}
